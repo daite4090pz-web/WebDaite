ece5966d8ad6b59d9e97b66cf6be599ff509deea
daite4090pz-web <daite4090.pz@gmail.com>
2026-04-27 20:54:21 +0300

    Delete hoi4wiki directory

--- hoi4wiki/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'zovovozTainiyKlyuch'
-    WTF_CSRF_SECRET_KEY = 'zovovozTainiyKlyuch'
-    SQLALCHEMYDATABASEURI = os.environ.get('DATABASEURL') or 'sqlite:///hoi4wiki.db'
-    SQLALCHEMYTRACKMODIFICATIONS = False
--- hoi4wiki/run.py
+++
@@ -1,8 +0,0 @@
-from app import create_app
-from app.models import globalInit
-
-app = create_app()
-
-if __name__ == '__main__':
-    globalInit()
-    app.run(debug=True)
